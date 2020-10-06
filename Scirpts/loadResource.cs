2b8bc4912e1e301be1493faf5778b07fe92a49ca
devillryu <sdryu1@hotmail.com>
2020-10-06 12:26:04 +0700

    playerpref

--- Scirpts/loadResource.cs
+++ Scirpts/loadResource.cs
@@ -22,6 +22,53 @@ public class loadResource : MonoBehaviour
         firstui = GameObject.Find("UI");
         backbutton = GameObject.Find("BackButton");
         backbutton.SetActive(false);
+        Loadoldcustomize();
+    }
+    void Loadoldcustomize()
+    {
+        hair = new List<GameObject>();
+        cloth = new List<GameObject>();
+        pant = new List<GameObject>();
+
+        //Load Old index
+        int oldhairindex = PlayerPrefs.GetInt("HairIndex");
+        int oldclothindex = PlayerPrefs.GetInt("ClothIndex");
+        int oldpantindex = PlayerPrefs.GetInt("PantIndex");
+
+        AllHairobj = Resources.LoadAll("Hair");
+        AllClothobj = Resources.LoadAll("Cloth");
+        AllPantobj = Resources.LoadAll("Pant");
+
+        hair_pos = transform.Find("/Player/Head/Hair_pos");
+        hair.Add(Resources.Load("Hair/Hair_01") as GameObject);
+        hair.Add(Resources.Load("Hair/Hair_02") as GameObject);
+        hair.Add(Resources.Load("Hair/Hair_03") as GameObject); //add here if have new Hair
+        newhair = Instantiate(hair[oldhairindex], hair_pos.transform.position, Quaternion.identity);
+        newhair.transform.parent = hair_pos.transform;
+        Havehair = true;
+
+        AllClothobj = Resources.LoadAll("Cloth");
+        cloth_pos = transform.Find("/Player/Cloth_pos");
+        cloth.Add(Resources.Load("Cloth/Cloth_01") as GameObject);
+        cloth.Add(Resources.Load("Cloth/Cloth_02") as GameObject);
+        cloth.Add(Resources.Load("Cloth/Cloth_03") as GameObject); //add here if have new Cloth
+        newcloth = Instantiate(cloth[oldclothindex], cloth_pos.transform.position, Quaternion.identity);
+        newcloth.transform.parent = cloth_pos.transform;
+        Havecloth = true;
+
+        AllPantobj = Resources.LoadAll("Pant");
+        pant_pos = transform.Find("/Player/Pant_pos");
+        pant.Add(Resources.Load("Pant/Pant_01") as GameObject);
+        pant.Add(Resources.Load("Pant/Pant_02") as GameObject);
+        pant.Add(Resources.Load("Pant/Pant_03") as GameObject); //add here if have new Pant
+        newpant = Instantiate(pant[oldpantindex], pant_pos.transform.position, Quaternion.identity);
+        newpant.transform.parent = pant_pos.transform;
+        Havepant = true;
+
+        //removeallList
+        hair.Clear();
+        cloth.Clear();
+        pant.Clear();
     }
     //Hair selection
     void CreateHairButton(string buttonname, int index)
@@ -41,6 +88,7 @@ public class loadResource : MonoBehaviour
             Havehair = false;
         }
         newhair = Instantiate(hair[index],hair_pos.transform.position,Quaternion.identity);
+        PlayerPrefs.SetInt("HairIndex", index);
         newhair.transform.parent = hair_pos.transform;
         Havehair = true;
     }
@@ -110,6 +158,7 @@ public class loadResource : MonoBehaviour
             Havecloth = false;
         }
         newcloth = Instantiate(cloth[index], cloth_pos.transform.position, Quaternion.identity);
+        PlayerPrefs.SetInt("ClothIndex", index);
         newcloth.transform.parent = cloth_pos.transform;
         Havecloth = true;
     }
@@ -155,6 +204,7 @@ public class loadResource : MonoBehaviour
             Havepant = false;
         }
         newpant = Instantiate(pant[index], pant_pos.transform.position, Quaternion.identity);
+        PlayerPrefs.SetInt("PantIndex", index);
         newpant.transform.parent = pant_pos.transform;
         Havepant = true;
     }
